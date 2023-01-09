80bfc18390f83ca9a414f8c498ea831a285c841a
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-09 20:57:20 -0300

    feat: persons migration

--- src/main/resources/db/migration/V02__CREATE_PERSONS.sql
+++ src/main/resources/db/migration/V02__CREATE_PERSONS.sql
@@ -1,5 +1,11 @@
 CREATE TABLE tb_persons(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    complement VARCHAR(255),
+    district VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    cep VARCHAR(255) NOT NULL,
     active BOOLEAN DEFAULT TRUE   
 );
