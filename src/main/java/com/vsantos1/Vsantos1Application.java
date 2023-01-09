381d68101ea60336c37daf97ca8cf93cba0eb586
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-08 23:21:22 -0300

    chore: db connection

---
+++ src/main/java/com/vsantos1/Vsantos1Application.java
@@ -0,0 +1,13 @@
+package com.vsantos1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vsantos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vsantos1Application.class, args);
+	}
+
+}
