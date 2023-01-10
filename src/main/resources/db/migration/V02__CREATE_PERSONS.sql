43f2a3a394c0a72d30d42913f9d03c1632c28514
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-09 21:07:41 -0300

    feat: person resource and migration number field

--- src/main/resources/db/migration/V02__CREATE_PERSONS.sql
+++ src/main/resources/db/migration/V02__CREATE_PERSONS.sql
@@ -5,7 +5,13 @@ CREATE TABLE tb_persons(
     complement VARCHAR(255),
     district VARCHAR(255) NOT NULL,
     city VARCHAR(255) NOT NULL,
+    number VARCHAR(255) NOT NULL,
     state VARCHAR(255) NOT NULL,
     cep VARCHAR(255) NOT NULL,
     active BOOLEAN DEFAULT TRUE   
 );
+
+INSERT INTO tb_persons (name, street, complement, district, city, number, state, cep) VALUES ('João da Silva', 'Rua das Flores', 'Apto 303', 'Jardim', 'Uberlândia', '1000', 'MG', '38400-000');
+INSERT INTO tb_persons (name, street, complement, district, city, number, state, cep) VALUES ('Maria de Souza', 'Avenida Matos', '105', 'Centro', 'São Paulo', '1000', 'SP', '38777-000');
+INSERT INTO tb_persons (name, street, complement, district, city, number, state, cep) VALUES ('Pedro Alves', 'Rua das Flores', 'Apto 303', 'Jardim', 'Uberlândia', '1000', 'MG', '38400-000');
+INSERT INTO tb_persons (name, street, complement, district, city, number, state, cep) VALUES ('Paulo Roberto', 'Avenida Floriano', '2106', 'Centro', 'Rio de Janeiro', '2000', 'RJ', '20000-000');
