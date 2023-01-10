8fa833e8dd6e055b10e067c283a230fcda37b67e
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-09 21:17:13 -0300

    feat: get by id method

--- src/main/java/com/vsantos1/resource/PersonResource.java
+++ src/main/java/com/vsantos1/resource/PersonResource.java
@@ -1,45 +1,55 @@
 package com.vsantos1.resource;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vsantos1.exception.ResourceNotFoundException;
 import com.vsantos1.model.Person;
 import com.vsantos1.repository.PersonRepository;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping(value = "/api/v1")
 public class PersonResource {
-    
+
     private final PersonRepository personRepository;
 
     public PersonResource(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-
     @GetMapping(value = "/persons")
-    public ResponseEntity<List<Person>> getAll(){
+    public ResponseEntity<List<Person>> getAll() {
         return ResponseEntity.status(HttpStatus.OK).body(personRepository.findAll());
     }
 
-    @PostMapping(value="/persons", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Person> createPerson (@RequestBody Person person) {
-        
-        Person entity = personRepository.save(person);
+    @GetMapping(value = "persons/{person_id}")
+    public ResponseEntity<Person> getById(@PathVariable("person_id") Long id) {
 
+        Optional<Person> optionalPerson = personRepository.findById(id);
+        if (optionalPerson.isEmpty()) {
+            throw new ResourceNotFoundException("No records found for this ID");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(optionalPerson.get());
+
+    }
+
+    @PostMapping(value = "/persons", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+
+        Person entity = personRepository.save(person);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(entity);
     }
-    
 
 }
