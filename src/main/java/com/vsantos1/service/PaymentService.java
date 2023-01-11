1ff5477db82c3e1ff9284a781bee4099b110c2e8
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-10 21:58:26 -0300

    feat: payment service

---
+++ src/main/java/com/vsantos1/service/PaymentService.java
@@ -0,0 +1,43 @@
+package com.vsantos1.service;
+
+import com.vsantos1.exception.ResourceNotFoundException;
+import com.vsantos1.model.Category;
+import com.vsantos1.model.Payment;
+import com.vsantos1.model.Person;
+import com.vsantos1.repository.CategoryRepository;
+import com.vsantos1.repository.PaymentRepository;
+import com.vsantos1.repository.PersonRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final CategoryRepository categoryRepository;
+
+    private final PersonRepository personRepository;
+
+
+    public PaymentService(PaymentRepository paymentRepository, CategoryRepository categoryRepository, PersonRepository personRepository) {
+        this.paymentRepository = paymentRepository;
+        this.categoryRepository = categoryRepository;
+        this.personRepository = personRepository;
+    }
+
+    public Payment execute(Payment payment) {
+        Optional<Category> optionalCategory = categoryRepository.findById(payment.getCategory().getId());
+        Optional<Person> optionalPerson = personRepository.findById(payment.getPerson().getId());
+
+        if (optionalCategory.isEmpty()) {
+            throw new ResourceNotFoundException("Category not found");
+        }
+
+        if (optionalPerson.isEmpty()) {
+            throw new ResourceNotFoundException("Person not found");
+        }
+
+        return paymentRepository.save(payment);
+    }
+}
