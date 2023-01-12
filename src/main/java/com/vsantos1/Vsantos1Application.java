2af7c1e2c011520aa492cc9ea22fc382640e7c79
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-12 02:16:28 -0300

    feat: in memory authentication

--- src/main/java/com/vsantos1/Vsantos1Application.java
+++ src/main/java/com/vsantos1/Vsantos1Application.java
@@ -2,12 +2,19 @@ package com.vsantos1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Vsantos1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Vsantos1Application.class, args);
-	}
+    public static void main(String[] args) {
+
+        // Only to get encrypted password on app startup
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        var test = bCryptPasswordEncoder.encode("password");
+        System.out.println(test);
+
+        SpringApplication.run(Vsantos1Application.class, args);
+    }
 
 }
--- src/main/java/com/vsantos1/security/SecurityConfig.java
+++ src/main/java/com/vsantos1/security/SecurityConfig.java
@@ -2,14 +2,16 @@ package com.vsantos1.security;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-
-public class SecurityConfig {
+@Configuration
+public class SecurityConfig  {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,9 +19,11 @@ public class SecurityConfig {
         http.cors()
                 .disable()
                 .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET).permitAll()
+                .authorizeHttpRequests()
                 .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .httpBasic();
 
