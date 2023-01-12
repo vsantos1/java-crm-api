0f2f46424cdaad1c07b6527fd9e2446f9cdc254f
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-12 01:20:21 -0300

    feat: spring security deps

---
+++ src/main/java/com/vsantos1/security/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.vsantos1.security;
+
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+
+public class SecurityConfig  {
+
+
+
+}
